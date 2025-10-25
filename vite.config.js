10682c082c023310ba7e9c54d17719b91c2fb41b
Wolf <wolf@example.com>
2025-10-25 20:15:53 +0200

    Fix overlay build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Vite-Konfiguration, die zwei HTML-Einstiegspunkte baut:
+// 1. index.html  (Hauptseite)
+// 2. public/overlay.html  (Overlay-Seite)
 export default defineConfig({
   plugins: [react()],
   build: {
