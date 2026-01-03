8a35b2dfd14cdc514e839cad0e9217b842e35179
Wolf <wolf@example.com>
2026-01-03 11:20:21 +0100

    Fix overlay build by moving html out of public

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        overlay: resolve(__dirname, "public/overlay.html"),
+        overlay: resolve(__dirname, "overlay.html"),
       },
     },
   },
